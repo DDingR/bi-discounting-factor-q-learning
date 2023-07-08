290af33d493882428b8ae2ad97ee953928067b3e
dding <msryu00@gmail.com>
2023-07-08 19:53:03 +0900

    IWIS fig added9

--- demo3/weight_visualize.m
+++ demo3/weight_visualize.m
@@ -17,33 +17,59 @@ close all
 % end
 
 %%
-NN_NAME =  "onnx/test_train9/0_4_0_start"; 
 
-fprintf("Loading Neural Network NN_NAME: %s\n", NN_NAME)    
-nn_full = "./" + NN_NAME + ".onnx";
-nn_full = importONNXNetwork( ...
-nn_full,  TargetNetwork="dlnetwork", InputDataFormats="BC", OutputDataFormats="BC" ...
-);
-% analyzeNetwork(nn)  
+TRAIN_NAME = "test_train9";
+
+NN_NAME_LIST = [
+    "0_1_0"
+    "0_5_0"
+    ];
+
+%%
+pst = ["start" "end"];
+
+for j = 1:1:size(NN_NAME_LIST, 1)
+
+    NN_NAME =  "onnx/" + TRAIN_NAME + "/" + NN_NAME_LIST(j, 1);
+
+    for k = 1:1:2
+
+    nn_full = "./" + NN_NAME + "_" + pst(k) + ".onnx";
+    nn_full = importONNXNetwork( ...
+    nn_full,  TargetNetwork="dlnetwork", InputDataFormats="BC", OutputDataFormats="BC" ...
+    );
+    % analyzeNetwork(nn)  
+    
+    weights = nn_full.Learnables.Value;
+    
+    [node_num, input_num] = size(weights{1});
+    output_num = size(weights{end}, 1);
+    layer_num = length(weights);
+    
+    weight_mat = [
+        weights{1} weights{2} weights{3} weights{4} weights{5}' [weights{6}; zeros(node_num-output_num, 1)]
+    ];
+    weight_mat = extractdata(weight_mat);
+    
+    figure(j)
+    subplot(2,1,k)
+    image(weight_mat, "CDataMapping","scaled")
+    colormap(jet(256));
+    colorbar
+%     clim([-0.5 0.5])
+    title(pst(k))
+
+    end
+
+end
 
 %%
 
-weights = nn_full.Learnables.Value;
+for j = 1:1:size(NN_NAME_LIST, 1)
+    saveas(figure(j), "weight" + string(j) + ".png")
+end
 
-[node_num, input_num] = size(weights{1});
-output_num = size(weights{end}, 1);
-layer_num = length(weights);
-
-weight_mat = [
-    weights{1} weights{2} weights{3} weights{4} weights{5}' [weights{6}; zeros(node_num-output_num, 1)]
-];
-weight_mat = extractdata(weight_mat);
-
-
-image(weight_mat, "CDataMapping","scaled")
-colormap(jet(256));
-colorbar
-clim([-0.5 0.5])
+%%
 
 % surf(weight_mat(:,20:end))
 % view([0,0,1])
